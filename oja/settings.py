bdf9b4499dc590a32e39ff7152dbc6b1454dd580
Emizy <adenitiree@gmail.com>
2017-12-08 12:49:35 +0100

    second comment

--- oja/settings.py
+++ oja/settings.py
@@ -138,7 +138,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-#offline setting
+# offline setting
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
